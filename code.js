dda05fa2b64a5d7064d9faf726ea840e3168ffd5
Julia <45842126+puphapp@users.noreply.github.com>
2020-03-23 15:40:33 +0300

    Create code.js

---
+++ code.js
@@ -0,0 +1,25 @@
+// #1
+
+var myName = prompt("Как тебя зовут?","");
+var myAge = prompt("Сколько тебе лет?","");
+  console.log("Меня зовут " + myName + ". Мне " + myAge);
+
+// #2
+ 
+var myName = prompt("Как тебя зовут?","");
+var myAge = prompt("Сколько тебе лет?","");
+   if(myName = "Ярополк", myAge >= 18) console.log("Приветствую тебя, друг!");
+     else console.log("Иди своей дорогой, путник...");
+     
+// #3
+
+var myName = prompt("Как тебя зовут?","");
+var arr =  whiteList["Зевс", "Геркулес", "Аид", "Дионис"];
+   if(myName = whitelist.some) console.log("Ключ от сейфа под столом");
+     else console.log("Иди своей дорогой, путник...");
+
+// #4
+// #5
+// #6
+// #7
+// #8
